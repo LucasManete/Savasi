ebf1dbeeda4bf4dae33f11e4762fe7fdfefacccb
LucasManete <74990186+LucasManete@users.noreply.github.com>
2023-03-19 11:27:56 -0300

    feat: initial crud for costumers,products and requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import { CostumersModule } from './costumers/costumers.module';
+import { ProductsModule } from './products/products.module';
+import { RequestsModule } from './requests/requests.module';
 
 @Module({
   imports: [
@@ -15,6 +18,9 @@ import { DataSource } from 'typeorm';
       synchronize: true,
       logging: ['query', 'error'],
     }),
+    CostumersModule,
+    ProductsModule,
+    RequestsModule,
   ],
 })
 export class AppModule {
